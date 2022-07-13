a9534090f155471ef527a2e86f97a9b956e5c470
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-07-13 10:48:39 -0300

    feat: criando context de autenticação

---
+++ src/context/auth.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface UserType {
+  id: string;
+  name: string;
+  cpfCnpj: string;
+  email: string;
+  whitelabelId: string;
+}
+
+interface AuthData {
+  user: UserType;
+  token: string;
+}
+
+interface AuthContextProps {
+  data: AuthData;
+  login: (credentials: LoginCredentials) => Promise<void>;
+  logout: () => void;
+}
+
+interface LoginCredentials {
+  email: string;
+  password: string;
+  whitelabelId: string;
+}
+const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+
+export function AuthContextProvider({ children }) {
+  const [data, setData] = useState<AuthData>(() => {
+    const token = localStorage.getItem('@urbisAPP:token');
+    const user = localStorage.getItem('@urbisAPP:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+    return {} as AuthData;
+  });
+
+  const login = async ({ email, password, whitelabelId }: LoginCredentials) => {
+    const response = await api.post('/auth/user', {
+      email,
+      password,
+      whitelabelId,
+    });
+
+    const { access_token, user } = response.data;
+    localStorage.setItem('@urbisAPP:token', access_token);
+    localStorage.setItem('@urbisAPP:user', JSON.stringify(user));
+
+    setData({ token: access_token, user });
+  };
+
+  const logout = (): void => {
+    localStorage.remove('@urbisAPP:token');
+    localStorage.remove('@urbisAPP:user');
+    setData({} as AuthData);
+  };
+
+  return (
+    <AuthContext.Provider value={{ data, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth(): AuthContextProps {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error('useAuth must be within an AuthProvider');
+  return context;
+}
