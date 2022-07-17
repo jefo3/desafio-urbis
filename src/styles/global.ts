c9fcfa1f5c5f00d7eb6c45acc5e9f7ca572ad452
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-07-16 22:20:05 -0300

    fix: arrumando o componente global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,19 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,900;1,400;1,500;1,700;1,900&display=swap');
   *{
     padding: 0;
     margin: 0;
     outline: 0;
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
   }
   body{
+    font-family: 'Roboto', sans-serif;
     background: #FCFCFC;
     color: #404041;
     -webkit-font-smoothing: antialiased;
   }
   body, input, button{
-    font-family:'Roboto Slab', serif;
     font-size: 16px;
   }
   h1, h2, h3, h4, h5, h6, strong{
