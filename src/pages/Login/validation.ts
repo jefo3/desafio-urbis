91121a75fdf5c945312a84a0a5417fe389c7211f
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-07-18 20:12:50 -0300

    fix: arrumando a validaçao

--- src/pages/Login/validation.ts
+++ src/pages/Login/validation.ts
@@ -5,7 +5,10 @@ const schema = yup.object().shape({
     .string()
     .email('Digite um email válido')
     .required('Email é obrigatorio'),
-  password: yup.string().min(6, 'Senha com minimo 6 caracteres'),
+  password: yup
+    .string()
+    .required('Senha é obrigatorio')
+    .min(6, 'Senha com minimo 6 caracteres'),
 });
 
 export default schema;
